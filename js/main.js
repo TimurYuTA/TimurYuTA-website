9252d523ac81a6a1765c8c384667b7f43a439bb5
Timur Yusupov <timuryuta@gmail.com>
2025-08-09 00:22:34 +0300

    added main.js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// =====   Theme - Dark / Light Mode   =====
+
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+// 1. Checking Dark Mode in user's setings of browser / OS
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+}
+
+// 2. Checking Dark Mode in Local Storage
+if (localStorage.getItem("darkMode") === "dark") {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+  btnDarkMode.classList.remove("dark-mode-btn--active");
+  document.body.classList.remove("dark");
+}
+
+// Switching the Mode of site if the Dark Mode has been changed in user's setings of browser / OS
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    const newColorScheme = event.matches ? "dark" : "light";
+
+    if (newColorScheme === "dark") {
+      btnDarkMode.classList.add("dark-mode-btn--active");
+      document.body.classList.add("dark");
+      localStorage.setItem("darkMode", "dark");
+    } else {
+      btnDarkMode.classList.remove("dark-mode-btn--active");
+      document.body.classList.remove("dark");
+      localStorage.setItem("darkMode", "light");
+    }
+  });
+
+// Switch Modes and save the selected mode to Local Storage
+btnDarkMode.onclick = function () {
+  btnDarkMode.classList.toggle("dark-mode-btn--active");
+  const isDark = document.body.classList.toggle("dark");
+
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+  } else {
+    localStorage.setItem("darkMode", "light");
+  }
+};
